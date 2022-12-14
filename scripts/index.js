a172d5d4054c261b499fdd3611b90e0d122bfd3d
Dasha Sedova <dashassed@gmail.com>
2022-12-14 23:30:33 +0300

    fix: поправить код после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,22 @@
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close-button');
-
-
-editButton.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    popup.classList.add('popup__opened');
-});
-
-function popupClosed() {
-    popup.classList.remove('popup__opened');
-}
-
-closeButton.addEventListener("click", popupClosed);
-
-const formElement = document.querySelector('.popup__container');
+const formElement = document.querySelector('.popup__form');
 const nameInput = formElement.querySelector('.popup__item_el_name');
 const descriptionInput = formElement.querySelector('.popup__item_el_description');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
+
+function popupOpened(evt) {
+    evt.preventDefault();
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    descriptionInput.value = profileDescription.textContent;
+}
+
+function popupClosed() {
+    popup.classList.remove('popup_opened');
+}
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
@@ -28,4 +26,6 @@ function handleFormSubmit (evt) {
     popupClosed();
 }
 
+editButton.addEventListener('click', popupOpened);
+closeButton.addEventListener('click', popupClosed);
 formElement.addEventListener('submit', handleFormSubmit);
