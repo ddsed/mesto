6b06aee10d06404631a54c3b646ddc7dc59c9c8e
Dasha Sedova <dashassed@gmail.com>
2023-01-11 21:43:36 +0300

    feat: finish step 3

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,69 @@ function handleFormSubmit (evt) {
 editButton.addEventListener('click', popupOpened);
 closeButton.addEventListener('click', popupClosed);
 formElement.addEventListener('submit', handleFormSubmit);
+
+const initialCards = [
+    {
+      name: 'Эльбрус',
+      link: './images/elbrus.jpeg'
+    },
+    {
+      name: 'Италия',
+      link: './images/Italy.jpeg'
+    },
+    {
+      name: 'Норвегия',
+      link: './images/norway.jpeg'
+    },
+    {
+      name: 'Байкал',
+      link: './images/baikal.jpeg'
+    },
+    {
+      name: 'Камчатка',
+      link: './images/kamchatka.jpeg'
+    },
+    {
+      name: 'Архыз',
+      link: './images/arkhyz.jpeg'
+    }
+  ];
+
+  const cardsContainer = document.querySelector('.elements__grid-container');
+  const cardsTemplate = document.querySelector('.cards-template').content.querySelector('.element');
+
+  function createCard({ name, link }) {
+    const card = cardsTemplate.cloneNode(true);
+    const cardName = card.querySelector('.element__title');
+    cardName.textContent = name;
+    const cardImage = card.querySelector('.element__photo');
+    cardImage.src = link; 
+
+    return card;
+  }
+
+  function renderCards() {
+    initialCards.forEach(item => {
+        const cardHtml = createCard(item);
+        cardsContainer.append(cardHtml);
+    });
+  }
+
+  renderCards();
+
+  const addButton = document.querySelector('.profile__add-button');
+  const popupAdd = document.querySelector('.popup-add');
+  const closeButtonPopupAdd = popupAdd.querySelector('.popup-add__close-button');
+
+  function popupAddOpened(evt) {
+    evt.preventDefault();
+    popupAdd.classList.add('popup-add_opened');
+  }
+
+  function popupAddClosed() {
+    popupAdd.classList.remove('popup-add_opened');
+  }
+
+  addButton.addEventListener('click', popupAddOpened);
+  closeButtonPopupAdd.addEventListener('click', popupAddClosed);
+
